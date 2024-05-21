c8033a6f4599fdc1e81b67bdad8e1bc72bbcb7ca
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-05-21 13:18:42 -0600

    Actualizar middleware de autenticación de la API para verificar el rol del usuario

--- app/Http/Middleware/AuthApiMiddleware.php
+++ app/Http/Middleware/AuthApiMiddleware.php
@@ -5,8 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class AuthApiMiddleware
-{
+class AuthApiMiddleware {
     /**
      * Handle an incoming request.
      *
@@ -14,14 +13,22 @@ class AuthApiMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(Request $request, Closure $next) {
         // Verificar si el token de la sesión está presente
         if (!$request->session()->has('token')) {
             // Si el token no está presente, redirigir al usuario a la página de inicio de sesión
             return redirect()->route('login')->with('error', 'Debes iniciar sesión para acceder a esta página.');
         }
+
+
+
+        // Verificar si el rol del usuario es '1' o '2'
+        $role = $request->session()->get('role');
+        if ($role !== '1' && $role !== '2') {
+            // Si el rol no es '1' o '2', redirigir al usuario a la página de inicio de sesión con un mensaje de error
+            return redirect()->route('login')->with('error', 'Rol no autorizado.');
+        }
+
         return $next($request);
     }
-
 }
