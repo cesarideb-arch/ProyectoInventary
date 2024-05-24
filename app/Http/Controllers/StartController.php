26968e85656595161c1969bcd9f64e0e1ff12575
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-05-24 13:00:54 -0600

    Actualizar carga perezosa de imágenes en la vista de índice de productos

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -9,29 +9,41 @@ class StartController extends Controller {
     public function index(Request $request) {
         // Obtener la URL base de la API desde la configuración
         $baseApiUrl = config('app.backend_api');
-
+    
         // Construir las URLs completas para las solicitudes API
         $apiUrl = $baseApiUrl . '/api/getCount';
         $apiUrlProducts = $baseApiUrl . '/api/getCountProducts';
-
+        $apiUrlEntrance = $baseApiUrl . '/api/GetProductEntrance';
+        $apiUrlOutput = $baseApiUrl . '/api/GetProductOutput';
+        $apiUrlLoan = $baseApiUrl . '/api/GetProductLoan';
+    
         // Obtener el token de la sesión
         $token = $request->session()->get('token');
-
-        // Realiza una solicitud HTTP GET a la API y obtén la respuesta
+    
+        // Realiza las solicitudes HTTP GET a la API y obtén las respuestas
         $response = Http::withToken($token)->get($apiUrl);
         $responseProducts = Http::withToken($token)->get($apiUrlProducts);
-
-        // Verifica si la solicitud fue exitosa
-        if ($response->successful() && $responseProducts->successful()) {
-            // Decodifica la respuesta JSON en un array asociativo
+        $responseEntrance = Http::withToken($token)->get($apiUrlEntrance);
+        $responseOutput = Http::withToken($token)->get($apiUrlOutput);
+        $responseLoan = Http::withToken($token)->get($apiUrlLoan);
+    
+        // Verifica si las solicitudes fueron exitosas
+        if ($response->successful() && $responseProducts->successful() &&
+            $responseEntrance->successful() && $responseOutput->successful() &&
+            $responseLoan->successful()) {
+            
+            // Decodifica las respuestas JSON en arrays asociativos
             $counts = $response->json();
             $products = $responseProducts->json();
-
-            // Pasa los datos de préstamos a la vista y renderiza la vista
-            return view('start.index', compact('counts', 'products'));
+            $entrances = $responseEntrance->json();
+            $outputs = $responseOutput->json();
+            $loans = $responseLoan->json();
+    
+            // Pasa los datos a la vista y renderiza la vista
+            return view('start.index', compact('counts', 'products', 'entrances', 'outputs', 'loans'));
         } else {
-            // Manejar el caso donde la solicitud no fue exitosa
+            // Manejar el caso donde alguna solicitud no fue exitosa
             return view('start.index', ['message' => 'Error al obtener datos de la API']);
         }
     }
-}
+}    
