038e213cf85c350e92a372ee825fdb18d9853179
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-06-14 11:38:46 -0600

    Actualizar carga perezosa de imágenes en la vista de préstamos y corregir estilos de botones

--- resources/views/loans/index.blade.php
+++ resources/views/loans/index.blade.php
@@ -21,11 +21,20 @@
             margin: auto;
         }
         .swal2-popup {
-            width: auto !important; /* Ajusta el ancho de la alerta */
-            max-width: 600px; /* Puedes ajustar este valor según tus necesidades */
-            padding: 20px; /* Ajusta el padding si es necesario */
+            width: 100% !important; /* Ajusta el ancho de la alerta */
+            max-width: 650px; /* Puedes ajustar este valor según tus necesidades */
+            padding: 40px; /* Ajusta el padding si es necesario */
+            text-align: center; /* Centrar el contenido del modal */
         }
-        
+        .swal2-icon {
+            margin: 20px auto; /* Centrar el icono dentro del modal */
+        }
+        .swal2-title {
+            text-align: center; /* Centrar el título */
+        }
+        .swal2-content {
+            text-align: center; /* Centrar el contenido del modal */
+        }
         .btn-custom-size {
             margin-right: 10px; /* Añade espacio a la derecha de cada botón excepto el último */
         }
@@ -65,7 +74,7 @@
                         <th>Producto</th>
                         <th>Responsable</th>
                         <th>Cantidad</th>
-                        <th>ubicación</th>
+                        <th>Ubicación</th>
                         <th>Observaciones</th>
                         <th>Fecha</th>
                         <th>Estado</th>
@@ -79,8 +88,7 @@
                         <td>{{ $loan['responsible'] }}</td>
                         <td>{{ number_format($loan['quantity'] ?? 'N/A', 0, '.', ',') }}</td>
                         <td>{{ $loan['product']['location'] ?? 'N/A' }}</td>
-
-                        <td>{{ $loan['observations'] ?? 'N/A' }}</td> 
+                        <td>{{ $loan['observations'] ?? 'N/A' }}</td>
                         <td>
                             @if($loan['status'] == 0)
                                 {{ $loan['updated_at'] ? \Carbon\Carbon::parse($loan['updated_at'])->setTimezone('America/Mexico_City')->format('Y-m-d H:i:s') : 'N/A' }}
@@ -130,121 +138,122 @@
     </div>
 
     <!-- Modal para enviar observaciones -->
-    <div class="modal fade" id="returnProductModal" tabindex="-1" role="dialog" aria-labelledby="returnProductModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="returnProductModalLabel">Devolver Producto</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="returnProductForm">
-                        @csrf
-                        @method('PUT')
-                        <div class="form-group">
-                            <label for="observations">Observaciones (Cambiar Opcional)</label>
-                            <textarea class="form-control" id="observations" name="observations" rows="3"></textarea>
-                        </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="noObservations">
-                            <label class="form-check-label" for="noObservations">No enviar observaciones</label>
-                        </div>
-                        <input type="hidden" id="loanId" name="loanId">
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary" id="confirmReturnProduct">Confirmar</button>
-                </div>
-            </div>
-        </div>
+    <div class="modal fade" id="returnProductModal" tabindex="-1" role="dialog" aria-labelledby
+    el="returnProductModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title" id="returnProductModalLabel">Devolver Producto</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">×</span>
+    </button>
     </div>
-
+    <div class="modal-body">
+    <form id="returnProductForm">
+    @csrf
+    @method('PUT')
+    <div class="form-group">
+    <label for="observations">Observaciones (Cambiar Opcional)</label>
+    <textarea class="form-control" id="observations" name="observations" rows="3"></textarea>
+    </div>
+    <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="noObservations">
+    <label class="form-check-label" for="noObservations">No enviar observaciones</label>
+    </div>
+    <input type="hidden" id="loanId" name="loanId">
+    </form>
+    </div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+    <button type="button" class="btn btn-primary" id="confirmReturnProduct">Confirmar</button>
+    </div>
+    </div>
+    </div>
+    </div>
+    
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            var currentLoanId = null;
-            var previousObservations = null;
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+    $(document).ready(function() {
+        var currentLoanId = null;
+        var previousObservations = null;
 
-            $('#noObservations').change(function() {
-                if($(this).is(':checked')) {
-                    $('#observations').val('');
-                } else {
-                    $('#observations').val(previousObservations);
+        $('#noObservations').change(function() {
+            if($(this).is(':checked')) {
+                $('#observations').val('');
+            } else {
+                $('#observations').val(previousObservations);
+            }
+        });
+
+        $('.return-product-btn').click(function() {
+            currentLoanId = $(this).data('loan-id');
+            previousObservations = $(this).data('loan-observations');
+            $('#loanId').val(currentLoanId); // Asigna el valor del ID del préstamo al campo oculto
+            $('#observations').val(previousObservations); // Carga las observaciones previas en el modal
+            $('#returnProductModal').modal('show');
+        });
+
+        $('#confirmReturnProduct').click(function() {
+            var observations = $('#noObservations').is(':checked') ? null : $('#observations').val();
+            var loanId = $('#loanId').val();
+
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: '¿Estás seguro de que deseas regresar este producto?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, regresar',
+                cancelButtonText: 'Cancelar',
+                background: '#fff',
+                customClass: {
+                    popup: 'swal2-popup'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                        url: '/loans/' + loanId,
+                        type: 'POST',
+                        data: {
+                            _method: 'PUT',
+                            _token: '{{ csrf_token() }}',
+                            loan_id: loanId,
+                            observations: observations // Enviar null si el checkbox está marcado
+                        },
+                        success: function(response) {
+                            Swal.fire({
+                                title: '¡Regresado!',
+                                text: 'El producto ha sido regresado exitosamente.',
+                                icon: 'success',
+                                background: '#fff',
+                                customClass: {
+                                    popup: 'swal2-popup'
+                                }
+                            }).then(() => {
+                                window.location.href = '{{ route('loans.index') }}';
+                            });
+                        },
+                        error: function(xhr, status, error) {
+                            Swal.fire({
+                                title: 'Error',
+                                text: 'Hubo un problema al regresar el producto.',
+                                icon: 'error',
+                                background: '#fff',
+                                customClass: {
+                                    popup: 'swal2-popup'
+                                }
+                            });
+                            console.error('Error al devolver el préstamo:', error);
+                        }
+                    });
                 }
             });
-
-            $('.return-product-btn').click(function() {
-                currentLoanId = $(this).data('loan-id');
-                previousObservations = $(this).data('loan-observations');
-                $('#loanId').val(currentLoanId); // Asigna el valor del ID del préstamo al campo oculto
-                $('#observations').val(previousObservations); // Carga las observaciones previas en el modal
-                $('#returnProductModal').modal('show');
-            });
-
-            $('#confirmReturnProduct').click(function() {
-                var observations = $('#noObservations').is(':checked') ? null : $('#observations').val();
-                var loanId = $('#loanId').val();
-
-                Swal.fire({
-                    title: '¿Estás seguro?',
-                    text: '¿Estás seguro de que deseas regresar este producto?',
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Sí, regresar',
-                    cancelButtonText: 'Cancelar',
-                    background: '#fff',
-                    customClass: {
-                        popup: 'swal2-popup'
-                    }
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        $.ajax({
-                            url: '/loans/' + loanId,
-                            type: 'POST',
-                            data: {
-                                _method: 'PUT',
-                                _token: '{{ csrf_token() }}',
-                                loan_id: loanId,
-                                observations: observations // Enviar null si el checkbox está marcado
-                            },
-                            success: function(response) {
-                                Swal.fire({
-                                    title: '¡Regresado!',
-                                    text: 'El producto ha sido regresado exitosamente.',
-                                    icon: 'success',
-                                    background: '#fff',
-                                    customClass: {
-                                        popup: 'swal2-popup'
-                                    }
-                                }).then(() => {
-                                    window.location.href = '{{ route('loans.index') }}';
-                                });
-                            },
-                            error: function(xhr, status, error) {
-                                Swal.fire({
-                                    title: 'Error',
-                                    text: 'Hubo un problema al regresar el producto.',
-                                    icon: 'error',
-                                    background: '#fff',
-                                    customClass: {
-                                        popup: 'swal2-popup'
-                                    }
-                                });
-                                console.error('Error al devolver el préstamo:', error);
-                            }
-                        });
-                    }
-                });
-            });
         });
-    </script>
-</body>
+    });
+</script>
+</body> 
 </html>
 @endsection
