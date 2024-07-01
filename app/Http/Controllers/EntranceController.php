67a1164045c28c212a19c8eabf92eb7a497e316e
Josue Montellano <2122100647@soy.utj.edu.mx>
2024-07-01 14:10:19 -0600

    Actualizar carga perezosa de imágenes en la vista de entradas y ajustar número máximo de elementos por página

--- app/Http/Controllers/EntranceController.php
+++ app/Http/Controllers/EntranceController.php
@@ -20,7 +20,7 @@ class EntranceController extends Controller {
         $searchQuery = $request->input('query');
     
         $page = $request->input('page', 1);
-        $perPage = 3;
+        $perPage = 100;
     
         $token = $request->session()->get('token');
     
