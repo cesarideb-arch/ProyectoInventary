57a1611ab971add7f7de27110e13df7dbb5abcd5
cesarideb-arch <cesarideb@gmail.com>
2025-10-07 15:11:41 -0600

    feat: update CORS and environment configuration for production

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,16 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:8000',
+        'http://localhost:80',
+        'https://attractive-balance-production.up.railway.app',
+        'https://proyectoinventary-production-5036.up.railway.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +21,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
