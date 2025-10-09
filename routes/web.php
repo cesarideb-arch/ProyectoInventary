c5a4897e7e416dd6b30ba2084360bff4d0a5c2e7
cesarideb-arch <cesarideb@gmail.com>
2025-10-09 14:28:45 -0600

    feat: add deployment configuration with seeders

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\DatabaseController;
 */
 
 Route::get('/', function () {
-    return view('login.index');
+    return view('/products');
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
