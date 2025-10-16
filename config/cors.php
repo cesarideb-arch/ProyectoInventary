b18f09d85c662454d641a08d4e7f790164b7f7d7
cesarideb-arch <cesarideb@gmail.com>
2025-10-16 13:20:48 -0600

    chore: temporary PHP 8.2 for local development

--- config/cors.php
+++ config/cors.php
@@ -7,13 +7,13 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:8000',
-        'http://localhost:80',
-        'https://attractive-balance-production.up.railway.app',
-        'https://proyectoinventary-production-2098.up.railway.app'
+        'https://apiinventario.idebmexico.com', // TU BACKEND
+        'https://proyectoinventario.idebmexico.com', // TU FRONTEND
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://.*\.idebmexico\.com',
+    ],
 
     'allowed_headers' => ['*'],
 
